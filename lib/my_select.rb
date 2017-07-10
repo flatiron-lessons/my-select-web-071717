115a3d8b4a998b5062d0cce332a4e610d7581bb1
James Miele <jnmiele@verizon.net>
2017-07-10 18:21:37 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
- # your code here!
+ 	for_true = []
+
+ 	collection.collect do |num|
+ 		if num.even? == true
+ 			for_true << num
+ 		end
+ 	end
+ 	for_true
 end
+
+
+#
+# Built-in Method
+#
+# collection.select do |num|
+#   num.even?
+# end
